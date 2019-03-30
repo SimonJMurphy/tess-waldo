877edf8eff8cdb2e0ee7e4f6b351f6fe9b1249a8
Simon Murphy <simonmurphy66@hotmail.com>
2019-03-29 21:41:18 -0700

    A description more befitting of the name.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='tess-waldo',
 version = '0.0.1',
-description = "A friendly package for visualising target locations on TESS CCDs.",
+description = "A friendly package for finding targets on TESS CCDs.",
 author = 'Simon J. Murphy',
 author_email = 'simon.murphy@sydney.edu.au',
 packages = ['tess_waldo'])
