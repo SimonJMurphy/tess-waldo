9930c4d0e968ac5cfdd35c29eb7a9cc1700b28ee
Simon Murphy <simonmurphy66@hotmail.com>
2019-04-02 16:42:59 +1100

    Need to bump version number to upload to pypi.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tess-waldo',
-version = '0.0.2',
+version = '0.0.3',
 description = "A friendly package for finding targets on TESS CCDs.",
 author = 'Simon J. Murphy',
 author_email = 'simon.murphy@sydney.edu.au',
